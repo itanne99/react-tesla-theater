2c322072a738041d7f830b0037811a394523424c
Ido Tanne <Idovader@gmail.com>
2023-04-12 10:28:49 -0400

    Added cookie support to hold onto theme type

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 import { ToggleThemeBtn } from './Components/ToggleThemeBtn';
 import { Home } from './Pages/Home';
 
 const App = () => {
-  const [theme, setTheme] = useState('light')
+  const [cookies, setCookie] = useCookies(['theme']);
+  const [theme, setTheme] = useState(cookies.theme || 'light');
+
+  useEffect(() => {
+    setCookie('theme', theme);
+  }, [theme, setCookie]);
 
   const toggleTheme = () => {
     setTheme((prevTheme) => {
