ecea7251dec766b433ff60b785bce1f5cf74a734
Ido Tanne <Idovader@gmail.com>
2023-04-12 09:59:22 -0400

    Added Tesla Font

--- src/Components/SiteHeader.js
+++ src/Components/SiteHeader.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import '../Styles/Components/SiteHeader.css'
 
 export const SiteHeader = () => {
+
   return (
     <Container>
-        <h1 className='text-center my-5'>Tesla Theater</h1>
+        <h1 className='tesla-font text-center my-5'>Tesla Theater</h1>
     </Container>
   )
 }
